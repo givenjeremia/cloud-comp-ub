7ea6874385476d44e5200a66a98200f3d6139dcd
Given Jeremia <jeremiagiven@gmail.com>
2024-09-02 21:17:15 +0700

    lah

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Comming Soon AJG!</h1>
+      <h1>Comming Soon AJG!</h1>
+
     </main>
   );
+
 }
