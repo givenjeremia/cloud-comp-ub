38888f90be96c81b79f6e0edf921455544cd4d84
Given Jeremia <jeremiagiven@gmail.com>
2024-09-08 21:39:39 +0700

    harapan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Comming Soon AJG!</h1>
-      <h2>Oke gas</h2>
-      <h1>Comming Soon AJG!</h1>
-
+      <h1>Comming Soon!</h1>
+      <h1>We On This!</h1>
+      <h1>Comming Soon!</h1>
     </main>
   );
 
