f1bf75391c2e5725d02a37ac94059c51c47fba7a
Given Jeremia <jeremiagiven@gmail.com>
2024-09-09 14:58:50 +0700

    components

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Comming Soon!</h1>
+      <h1>We On This!</h1>
+      <h1>Comming Soon!</h1>
+    </main>
+  );
+
+}
