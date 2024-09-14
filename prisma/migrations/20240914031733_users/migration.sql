aa621c15f77c149bf29887a016c8b50addcc3603
Given Jeremia <jeremiagiven@gmail.com>
2024-09-14 10:37:07 +0700

    setup db

---
+++ prisma/migrations/20240914031733_users/migration.sql
@@ -0,0 +1,61 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `phonenumber` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `otps` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` INTEGER NOT NULL,
+    `otp` VARCHAR(191) NOT NULL,
+    `type` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    INDEX `otps_userId_idx`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `tokens` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` INTEGER NOT NULL,
+    `accessToken` VARCHAR(191) NOT NULL,
+    `refreshToken` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    INDEX `tokens_userId_idx`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `baby_names` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `uuid` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `gender` TEXT NOT NULL,
+    `meaning` TEXT NOT NULL,
+    `origin` TEXT NOT NULL,
+    `sub_origin` TEXT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `baby_names_uuid_key`(`uuid`),
+    UNIQUE INDEX `baby_names_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `otps` ADD CONSTRAINT `otps_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `tokens` ADD CONSTRAINT `tokens_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ prisma/seeders/BabySeeder.ts
@@ -0,0 +1,75 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+import { default as baby_data } from "./data/excel-to-json.json";
+
+interface BabyName {
+    name: string;
+    gender: string;
+    meaning: string;
+    origin: string;
+    sub_origin: string;
+}
+
+class BabyNameSeeder {
+    private prisma: PrismaClient;
+
+    constructor() {
+        this.prisma = new PrismaClient();
+    }
+
+    public async run(): Promise<void> {
+        const coaDataArray = baby_data as BabyName[];
+        let successCount = 0;
+        const operationStartTime = new Date();
+    
+        for (const coa of coaDataArray) {
+            const currentTime = new Date();
+            if (coa.gender && coa.gender !== 'Gender') {
+                try {
+                    const insertStartTime = new Date();
+                    
+                    let baby = await this.prisma.babyName.upsert({
+                        where: { name: coa.name },
+                        update: {
+                            name: coa.name,
+                            gender: coa.gender,
+                            meaning: coa.meaning,
+                            origin: coa.origin,
+                            sub_origin: coa.sub_origin,
+                        },
+                        create: {
+                            name: coa.name,
+                            gender: coa.gender,
+                            meaning: coa.meaning,
+                            origin: coa.origin,
+                            sub_origin: coa.sub_origin,
+                        },
+                    });
+    
+                    const insertEndTime = new Date(); 
+                    const insertionTime = insertEndTime.getTime() - insertStartTime.getTime();
+    
+                    console.log(`Success Insert: ${baby.name}, Time taken: ${insertionTime} ms`);
+                    successCount++;
+                } catch (error) {
+                    console.error('Error inserting baby name:', error);
+                }
+            } else {
+                console.log('Skipped: ' + coa.name);
+                await new Promise(resolve => setTimeout(resolve, 10000));
+            }
+        }
+    
+        const operationEndTime = new Date();
+        const totalTime = operationEndTime.getTime() - operationStartTime.getTime();
+    
+        if (successCount === coaDataArray.length) {
+            console.log('All upsert operations were successful.');
+        } else {
+            console.log('Some upsert operations were skipped or failed.');
+        }
+        console.log(`Total time taken for all operations: ${totalTime} ms`);
+    }
+    
+}
+
+export default new BabyNameSeeder();
