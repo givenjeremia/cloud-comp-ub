367b9f9557bf7f6d6edcfeb10e862730df383708
Given Jeremia <jeremiagiven@gmail.com>
2024-12-09 19:18:38 +0700

    cok

--- src/controllers/BabyController.ts
+++ src/controllers/BabyController.ts
@@ -26,6 +26,12 @@ class BabyController extends Controller {
         this.router.get("/data-by-like", this.DataByLike);
         this.router.post("/random-baby-name",this.validateGetRandomBabyName, this.getRandomBabyName);
         this.router.put("/like-baby-name/:uuid", this.likeBabyName);
+
+        // UAS
+        this.router.get("/get-baby-name-by-origin", this.getBabyNameByOrigin);
+        this.router.get("/get-baby-name-first-letter", this.getBabyNameFirstLetter);
+        this.router.get("/get-baby-name-meaning", this.getBabyNameMeaning);
+
     }
 
 
@@ -184,6 +190,188 @@ class BabyController extends Controller {
                 },
             });
             return super.success(res, "success updated", data);
+        } catch (error: any) {
+            console.error(error.message);
+            return super.error(res, error.message);
+        }
+    }
+
+
+    // UAS
+    public async getBabyNameByOrigin(req: Request, res: Response): Promise<Response> {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) return super.badRequest(res, "invalid request", errors.array());
+    
+            const origin = req.query.origin as string || 'all';
+    
+            const page = parseInt(req.query.page as string) || 1;
+            const pageSize = parseInt(req.query.pageSize as string) || 10;
+            
+            // Filter conditions
+            const whereConditions = {
+                ...(origin !== 'all' ? { origin } : {}),
+            };
+    
+            // Get total count for pagination
+            const totalCount = await prisma.babyName.count({ where: whereConditions });
+            const totalPages = Math.ceil(totalCount / pageSize);
+    
+            // Fetch paginated data
+            const babyNames = await prisma.babyName.findMany({
+                where: whereConditions,
+                orderBy: {
+                    like: {
+                        sort: 'desc',
+                    }
+                },
+                take: pageSize,
+                skip: (page - 1) * pageSize,
+            });
+    
+            // Generate pagination links
+            const createLink = (newPage: number) => {
+                const query = new URLSearchParams(req.query as any);
+                query.set('page', newPage.toString());
+                query.set('pageSize', pageSize.toString());
+                return `?${query.toString()}`; 
+            };
+    
+            const nextLink = page < totalPages ? createLink(page + 1) : null;
+            const prevLink = page > 1 ? createLink(page - 1) : null;
+    
+            // Response
+            return super.success(res, "success", {
+                data: babyNames,
+                pagination: {
+                    currentPage: page,
+                    pageSize,
+                    totalCount,
+                    totalPages,
+                    nextLink,
+                    prevLink,
+                },
+            });
+        } catch (error: any) {
+            console.error(error.message);
+            return super.error(res, error.message);
+        }
+    }
+
+    public async getBabyNameFirstLetter(req: Request, res: Response): Promise<Response> {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) return super.badRequest(res, "invalid request", errors.array());
+    
+            const firstLetter = req.query.firstLetter as string || null;
+    
+            const page = parseInt(req.query.page as string) || 1;
+            const pageSize = parseInt(req.query.pageSize as string) || 10;
+            
+            // Filter conditions
+            const whereConditions = {
+                ...(firstLetter && firstLetter !== 'all' ? { name: { startsWith: firstLetter } } : {}),
+            };
+    
+            // Get total count for pagination
+            const totalCount = await prisma.babyName.count({ where: whereConditions });
+            const totalPages = Math.ceil(totalCount / pageSize);
+    
+            // Fetch paginated data
+            const babyNames = await prisma.babyName.findMany({
+                where: whereConditions,
+                orderBy: {
+                    like: {
+                        sort: 'desc',
+                    }
+                },
+                take: pageSize,
+                skip: (page - 1) * pageSize,
+            });
+    
+            // Generate pagination links
+            const createLink = (newPage: number) => {
+                const query = new URLSearchParams(req.query as any);
+                query.set('page', newPage.toString());
+                query.set('pageSize', pageSize.toString());
+                return `?${query.toString()}`; 
+            };
+    
+            const nextLink = page < totalPages ? createLink(page + 1) : null;
+            const prevLink = page > 1 ? createLink(page - 1) : null;
+    
+            // Response
+            return super.success(res, "success", {
+                data: babyNames,
+                pagination: {
+                    currentPage: page,
+                    pageSize,
+                    totalCount,
+                    totalPages,
+                    nextLink,
+                    prevLink,
+                },
+            });
+        } catch (error: any) {
+            console.error(error.message);
+            return super.error(res, error.message);
+        }
+    }
+
+    public async getBabyNameMeaning(req: Request, res: Response): Promise<Response> {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) return super.badRequest(res, "invalid request", errors.array());
+    
+            const meaning = req.query.meaning as string || null;
+    
+            const page = parseInt(req.query.page as string) || 1;
+            const pageSize = parseInt(req.query.pageSize as string) || 10;
+            
+            // Filter conditions
+            const whereConditions = {
+                ...(meaning ? { meaning: { contains: meaning } } : {}),
+            };
+    
+            // Get total count for pagination
+            const totalCount = await prisma.babyName.count({ where: whereConditions });
+            const totalPages = Math.ceil(totalCount / pageSize);
+    
+            // Fetch paginated data
+            const babyNames = await prisma.babyName.findMany({
+                where: whereConditions,
+                orderBy: {
+                    like: {
+                        sort: 'desc',
+                    }
+                },
+                take: pageSize,
+                skip: (page - 1) * pageSize,
+            });
+    
+            // Generate pagination links
+            const createLink = (newPage: number) => {
+                const query = new URLSearchParams(req.query as any);
+                query.set('page', newPage.toString());
+                query.set('pageSize', pageSize.toString());
+                return `?${query.toString()}`; 
+            };
+    
+            const nextLink = page < totalPages ? createLink(page + 1) : null;
+            const prevLink = page > 1 ? createLink(page - 1) : null;
+    
+            // Response
+            return super.success(res, "success", {
+                data: babyNames,
+                pagination: {
+                    currentPage: page,
+                    pageSize,
+                    totalCount,
+                    totalPages,
+                    nextLink,
+                    prevLink,
+                },
+            });
         } catch (error: any) {
             console.error(error.message);
             return super.error(res, error.message);
--- src/middlewares/ApiKeyMiddleware.ts
+++ src/middlewares/ApiKeyMiddleware.ts
@@ -3,15 +3,15 @@ import type { Request, Response, NextFunction } from "express";
 
 const ApiKeyMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const key = req.headers["api-key"];
-    console.log(key)
-    console.log(process.env.API_KEY)
-    if (key !== process.env.API_KEY) {
-        return res.status(403).json({
-            data: null,
-            message: "Forbidden",
-            status: 403,
-        });
-    }
+    // console.log(key)
+    // console.log(process.env.API_KEY)
+    // if (key !== process.env.API_KEY) {
+    //     return res.status(403).json({
+    //         data: null,
+    //         message: "Forbidden",
+    //         status: 403,
+    //     });
+    // }
     next();
 };
 
